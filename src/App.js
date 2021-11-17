7d8eba07d7be9a1fb27ab9388549ac2fddaedd4c
Tapan Siwach <siwach.tapan@gmail.com>
2021-11-17 08:13:08 -0500

    add routes for Dashboard, Login, Register & Reset

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Dashboard from './components/Dashboard';
+import Login from './components/Login';
+import Register from './components/Register';
+import Reset from './components/Reset';
 
 function App() {
   return (
     <div className="App">
-
+      <Router>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/reset" element={<Reset />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
