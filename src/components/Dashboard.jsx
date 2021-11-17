072435cba7af4b8fff67da6965484072871f4365
Tapan Siwach <siwach.tapan@gmail.com>
2021-11-17 11:22:17 -0500

    layout Dashboard component

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -5,7 +5,18 @@ function Dashboard() {
   return (
     <div className="dashboard">
       <div className="dashboard__container">
-        Dashboard
+        <div className="dashboard__sidenav">
+          Side Nav
+          <ul>
+            <li>List Item 1</li>
+            <li>List Item 2</li>
+            <hr />
+            <li><button className="logout__button">Logout</button></li>
+          </ul>
+        </div>
+        <div className="dashboard__mainContent">
+          Main Content
+        </div>
       </div>
     </div>
   )
