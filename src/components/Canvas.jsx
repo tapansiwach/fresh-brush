74befcd58017a7ab2ed67a47cc78925766049e65
Tapan Siwach <siwach.tapan@gmail.com>
2021-11-19 03:59:07 -0500

    add event listeners to canvas for mouse up, down & move events

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -2,9 +2,25 @@ import React from 'react'
 import './Canvas.scss';
 
 function Canvas() {
+  const startDrawing = (e) => {
+    console.log("starting to draw");
+    console.log(`e`, e);
+  }
+
+  const draw = (e) => {
+    console.log("draw");
+    console.log(`e`, e);
+  }
+
+  const finishDrawing = (e) => {
+    console.log("finish drawing")
+  }
   return (
     <canvas
       id="canvas"
+      onMouseDown={e => startDrawing(e)}
+      onMouseMove={e => draw(e)}
+      onMouseUp={e => finishDrawing(e)}
     >
       your browser doesn't support canvas
     </canvas>
