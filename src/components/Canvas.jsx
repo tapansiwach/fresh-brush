d15f1e3f1b73b770c8924f61f55742b1fb018bf7
Tapan Siwach <siwach.tapan@gmail.com>
2021-11-19 05:15:41 -0500

    allow user to draw on canvas by clicking and dragging their mouse

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,27 +1,37 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './Canvas.scss';
 
 function Canvas() {
   const canvasRef = useRef(null);
   const contextRef = useRef(null);
+  const [isDrawing, setIsDrawing] = useState(false);
 
   useEffect(() => {
     const context = canvasRef.current.getContext("2d");
+    context.lineJoin = "round";
+    context.lineCap = "round";
     contextRef.current = context;
   }, [])
 
-  const startDrawing = (e) => {
-    console.log("starting to draw");
-    console.log(`e`, e);
+  const startDrawing = ({ nativeEvent }) => {
+    const { offsetX, offsetY } = nativeEvent;
+    const [x, y] = [offsetX / devicePixelRatio, offsetY / devicePixelRatio];
+    contextRef.current.beginPath();
+    contextRef.current.moveTo(x, y);
+    setIsDrawing(true);
   }
 
   const draw = (e) => {
-    console.log("draw");
-    console.log(`e`, e);
+    if (!isDrawing) return;
+    const { offsetX, offsetY } = e.nativeEvent;
+    const [x, y] = [offsetX / devicePixelRatio, offsetY / devicePixelRatio];
+    contextRef.current.lineTo(x, y);
+    contextRef.current.stroke();
   }
 
   const finishDrawing = (e) => {
-    console.log("finish drawing")
+    contextRef.current.closePath();
+    setIsDrawing(false);
   }
   return (
     <canvas
