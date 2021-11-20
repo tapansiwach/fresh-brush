605ac8478849de0aac0d80de57833979543f5cc7
Tapan Siwach <siwach.tapan@gmail.com>
2021-11-20 06:55:23 -0500

    code cleanup: address react warnings

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from '../firebase/config';
-import { createImageDocInDB } from '../firebase/db';
 import { saveImageToStorage } from '../firebase/storage';
 import './Canvas.scss';
 
--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,20 +1,22 @@
 import React, { useEffect, useState } from 'react'
-import { auth } from '../firebase/config';
 import { getImageDocs } from '../firebase/db';
 import './Gallery.scss';
 
 function Gallery({ uid }) {
   const [urls, setUrls] = useState([]);
 
-  useEffect(async () => {
-    const snap = await getImageDocs(uid);
-    const allUrls = []
-    snap.forEach(doc => {
-      const aUrl = doc.data().downloadURL;
-      allUrls.push(aUrl);
-    });
-    setUrls(allUrls);
-  }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      const snap = await getImageDocs(uid);
+      const allUrls = []
+      snap.forEach(doc => {
+        const aUrl = doc.data().downloadURL;
+        allUrls.push(aUrl);
+      });
+      setUrls(allUrls);
+    }
+    fetchData();
+  }, [uid]);
 
 
   return (
